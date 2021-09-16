a6402fc446fe2bc59393da3f182f23438737a28c
gpereira97 <gabrielpereira762@gmail.com>
2021-09-16 19:10:20 -0300

    add check-node

---
+++ check-node/files/create.js
@@ -0,0 +1,36 @@
+var XMLHttpRequest = require('xhr2');
+var xhr = new XMLHttpRequest();
+ 
+const base_url = "https://60d07a397de0b20017108cab.mockapi.io/";
+
+const request = obj => {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open(obj.method || "GET", obj.url);
+        if (obj.headers) {
+            Object.keys(obj.headers).forEach(key => {
+                xhr.setRequestHeader(key, obj.headers[key]);
+            });
+        }
+        xhr.responseType = 'json';
+        xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr);
+            } else {
+                reject(xhr);
+            }
+        };
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send(obj.body);
+    });
+};
+
+request({url: `${base_url}broker/instance`, method: "POST"})
+.then(data => {
+    item = data.response;
+    item.status = data.status;
+    console.log(JSON.stringify(data.response));
+})
+.catch(error => {
+    console.log(JSON.stringify({ status: error.status }));
+});
