6d19f98d329b526c356730d156711be6fbae3f16
gpereira97 <66231529+gpereira97@users.noreply.github.com>
2021-09-18 00:13:44 -0300

    update

--- start-vm/teste.sh
+++ start-vm/teste.sh
@@ -2,5 +2,5 @@
 
 cat roles/start-vm/templates/start-vm.json.old > roles/start-vm/templates/start-vm.json
 
-UUID=$(cat /root/vmware-novo/clone/result.txt | awk '{print $1}')
+UUID=$(cat results/result.txt | awk '{print $1}')
 sed -i "s/UUID/$UUID/" roles/start-vm/templates/start-vm.json
