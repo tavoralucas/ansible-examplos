d8f030660d193c38b99664e5bcbca2e2ebb64572
gpereira97 <66231529+gpereira97@users.noreply.github.com>
2021-09-18 00:12:51 -0300

    update teste

--- network/teste.sh
+++ network/teste.sh
@@ -2,5 +2,5 @@
 
 cat roles/network/templates/net-teste.json.old > roles/network/templates/net-teste.json
 
-UUID=$(cat /results/result.txt | awk '{print $1}')
+UUID=$(cat results/result.txt | awk '{print $1}')
 sed -i "s/UUID/$UUID/" roles/network/templates/net-teste.json
