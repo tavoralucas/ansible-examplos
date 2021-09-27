50476b0c6b6df18b3c42cb260e2a2bf757beb3b0
monickemontenegro <monicke.montenegro@gmail.com>
2021-09-27 10:07:33 -0300

    Change names

--- terraform/run-cloudformation/main.tf
+++ terraform/run-cloudformation/main.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudformation_stack" "this" {
-  name = "create-s3-cf-terraform"
+  name = "create-s3-cf-terraform-bb"
   template_body = file("${path.module}/files/s3-bucket.yml")
 }
