55e6202f631cbedc3fd9d82f63590c94e90d40c8
Monicke <monicke.montenegro@gmail.com>
2022-03-31 12:51:11 -0300

    Adicionando Power Shell no mesmo Playbook do Linux para reaproveitar o Suffix

---
+++ azure2/azure.ps1
@@ -0,0 +1,8 @@
+$credspath = "./file"
+$envname=$args[1]
+$asrvaultname = "pmc-vault-$envname"
+$cert = New-SelfSignedCertificate -certstorelocation $credspath -FriendlyName 'test-vaultcredentials'  -subject "Windows Azure Tools" -KeyExportPolicy Exportable -NotAfter $(Get-Date).AddHours(48) -NotBefore $(Get-Date).AddHours(-24) -KeyProtection None -KeyUsage None -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2") -Provider "Microsoft Enhanced Cryptographic Provider v1.0"
+$certificate = [System.Convert]::ToBase64String($cert.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pfx))
+$vault1 = Get-AzRecoveryServicesVault -Name $asrvaultname
+$credsfilename = Get-AzRecoveryServicesVaultSettingsFile -Backup -Vault $vault1 -Path $credspath -Certificate $certificate
+$credsfilename
